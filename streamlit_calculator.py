0f1ef62a05420543c41491caeec843d9356a09b5
SAMET GİRGİN <44918645+sametgirgin@users.noreply.github.com>
2025-03-27 16:43:18 +0300

    Update streamlit_calculator.py

--- streamlit_calculator.py
+++ streamlit_calculator.py
@@ -196,9 +196,9 @@ with col4:
     st.button(")", key="close_paren", on_click=lambda: add_to_display(")"))
     st.button("MC", key="memory_clear", on_click=memory_clear)
     st.button("/", key="divide", on_click=lambda: add_to_display("/"))
-    st.button("* ", key="multiply", on_click=lambda: add_to_display("*"))
-    st.button("- ", key="subtract", on_click=lambda: add_to_display("-"))
-    st.button("+ ", key="add", on_click=lambda: add_to_display("+"))
+    st.button(" * ", key="multiply", on_click=lambda: add_to_display("*"))
+    st.button(" - ", key="subtract", on_click=lambda: add_to_display("-"))
+    st.button(" + ", key="add", on_click=lambda: add_to_display("+"))
 # Add instructions
 st.markdown("""
     ### Instructions:
